cbd79d7b6edf1f6176a5a12e43dd7eb1a553189c
Mohit_Gupta <52665879+Mohit-15@users.noreply.github.com>
2020-06-29 16:40:00 +0530

    Update settings.py

--- Broadcast_Sending/settings.py
+++ Broadcast_Sending/settings.py
@@ -126,5 +126,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'msbproject1234@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjvimualxfovfxbu'
+EMAIL_HOST_USER = 'xxxxxxxxxxxxxxxxxx'
+EMAIL_HOST_PASSWORD = 'xxxxxx'
