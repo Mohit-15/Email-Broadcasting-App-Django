79e11f7e985592cbe4ed07a324fd0462d27b65a7
Mohit-15 <programs.mohit15@gmail.com>
2020-09-09 12:33:35 +0530

    added verbose in models.py

--- Broadcasting_email/models.py
+++ Broadcasting_email/models.py
@@ -7,12 +7,20 @@ class EmailDetail(models.Model):
 	email = models.EmailField(_('email'),max_length = 30, unique = True)
 	name = models.CharField(_('name'), max_length= 20)
 
+	class Meta:
+		verbose_name = _('EmailDetail')
+		verbose_name_plural = _('EmailDetails')
+
 	def __str__(self):
 		return self.email
 
 class Message(models.Model):
 	message = models.TextField(_('message'), blank=True, null=True)
 	name = models.CharField(_('name'), max_length= 20)
+
+	class Meta:
+		verbose_name = _('Message')
+		verbose_name_plural = _('Messages')
 
 	def __str__(self):
 		return self.name
